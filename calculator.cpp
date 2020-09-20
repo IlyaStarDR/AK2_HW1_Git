25102c671bf9770b0d38f825bee10e780a85a14d
ddanilyuk <danis.danilyuk@gmail.com>
2020-09-21 00:52:06 +0300

    Comments added. Header and function fixed

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,19 @@
 #include "calculator.h"
 
+// Addition function
 int Calculator::Add (double a, double b)
 {
-	return a + b + 0.5;
+	return a + b;
 }
 
+// Subtraction function
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+// Multiplication function
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.5;
+    return a * b;
 }
